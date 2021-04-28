7a219ceabe8ff72236d1b8b4e0b2e4300f8eca33
victoria-tkachenko <victoria-malysheva@yandex.ru>
2021-04-28 08:16:40 +0300

    Commit Lesson3

---
+++ src/test/java/lesson3/Start.java
@@ -0,0 +1,17 @@
+package lesson3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Start {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        Thread.sleep(5000);
+
+        driver.close();
+    }
+}
