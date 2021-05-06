5509948f1c5f1c22a04f4b573b5943c9838e7765
victoria-tkachenko <victoria-malysheva@yandex.ru>
2021-05-06 08:03:17 +0300

    Commit HW4

---
+++ src/main/java/homework4/Functions.java
@@ -0,0 +1,38 @@
+package homework4;
+
+
+public class Functions {
+    public boolean isNumberEven(Integer number) {
+        return number % 2 == 0;
+    }
+
+    public boolean isNumberPositive(Integer number) {
+        return number > 0;
+    }
+
+    public boolean isPrime(Integer number) {
+        if (number < 0) {
+            return false;
+        } else if (number == 1) {
+            return true;
+        } else {
+            for (int i = 2; i < number - 1; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String word) {
+        if (word.length() < 2) {
+            return true;
+        }
+        if (word.charAt(0) != word.charAt(word.length() - 1)) {
+            return false;
+        }
+        return isPalindrome(word.substring(1, word.length() - 1));
+    }
+
+}
---
+++ src/test/java/homework4/FunctionCalculateTriangleSquareTest.java
@@ -0,0 +1,47 @@
+package homework4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assumptions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FunctionCalculateTriangleSquareTest {
+
+    FunctionCalculateTriangleSquare functionCalculateTriangleSquare = new FunctionCalculateTriangleSquare();
+    private static Logger logger = LoggerFactory.getLogger(FunctionCalculateTriangleSquareTest.class);
+
+
+    @Test
+    void testIsTrianglePositive() throws NotTriangleException {
+        logger.debug("Позитивная проверка верными значениями");
+        assertThat(functionCalculateTriangleSquare.calculateTriangleSquare(2,2,2)).isNotNegative();
+    }
+
+    @Test
+    void checkExceptionWhenNotTriangleZero() {
+        logger.debug("Проверка выброса исключения при передаче нулевых значений");
+        assertThatExceptionOfType(NotTriangleException.class).isThrownBy(() ->
+                functionCalculateTriangleSquare.calculateTriangleSquare(0,0,0));
+    }
+
+    @Test
+    void checkExceptionWhenNotTriangleNegativeSide(){
+        logger.debug("Проверка выброса исключения при передаче отрицательного значения");
+        assertThatExceptionOfType(NotTriangleException.class).isThrownBy(() ->
+                functionCalculateTriangleSquare.calculateTriangleSquare(-2, 2, 2));
+    }
+
+    @Test
+    void checkExceptionWhenNotTriangleTwoSidesLess() {
+        logger.debug("Проверка выброса исключения в случае если две стороны меньше третьей");
+        assertThatExceptionOfType(NotTriangleException.class).isThrownBy(() ->
+                functionCalculateTriangleSquare.calculateTriangleSquare(2, 7, 10));
+    }
+
+}
